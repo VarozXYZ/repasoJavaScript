8a546fbcca80ed93f3e255fc7280e1027736cbce
VarozXYZ <alvaropiro@gmail.com>
2025-12-02 10:31:12 +0100

    ej6

--- 6.js
+++ 6.js
@@ -7,3 +7,20 @@
 
 /* num = 153
 num = 123 */ 
+
+function amstrong(n) {
+    let original = n.toString().split("");
+    let amstrong = 0;
+    original.forEach(num => {
+        amstrong += parseInt(num) ** original.length;
+    });
+    if (n == amstrong) {
+        console.log("El número es Amstrong");
+        return true;
+    } else {
+        console.log("El número no es Amstrong");
+        return false;
+    }
+}
+
+amstrong(153)
