3fb0521c9747777c44eb4643e1e5da47a10a6f9a
VarozXYZ <alvaropiro@gmail.com>
2025-12-02 10:39:20 +0100

    ej8

--- 8.js
+++ 8.js
@@ -6,3 +6,12 @@
 /* convertirAMilisegundos(dias, horas, minutos, segundos)
 dias = 1, horas = 0, minutos = 0, segundos = 0
 86400000 ms */
+
+function convertirAMilisegundos(dias, horas, minutos, segundos) {
+    horas += dias * 24;
+    minutos += horas * 60;
+    segundos += minutos * 60;
+    return segundos * 1000;
+}
+
+console.log(convertirAMilisegundos(1, 0, 0, 0));
