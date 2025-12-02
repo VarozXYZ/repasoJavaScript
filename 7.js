70ca1f03188171d602e52d15e9e1c88029049f13
VarozXYZ <alvaropiro@gmail.com>
2025-12-02 10:37:20 +0100

    ej7

--- 7.js
+++ 7.js
@@ -8,3 +8,16 @@
 /* capitalizarPalabras(texto)
 texto = "hola mundo"
 Salia: Hola Mundo */ 
+
+function capitalizarPalabras(text) {
+    let wordArray = text.split(" ");
+    wordArray = wordArray.map(word => {
+        let letters = word.split("");
+        letters[0] = letters[0].toUpperCase();
+        return letters.join("");
+    });
+    wordArray = wordArray.join(" ");
+    return wordArray;
+}
+
+console.log(capitalizarPalabras("hola mundo"));
