9113951b0fd99e77938c5c403bfd206a9d783f6a
VarozXYZ <alvaropiro@gmail.com>
2025-12-02 09:47:06 +0100

    ej2

--- 2.js
+++ 2.js
@@ -3,3 +3,15 @@
  * sin usar funciones propias del lenguaje que lo hagan de forma automática.
  * - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
  */
+
+function textInverter(text) {
+    const letters = text.split("");
+    let invertedText = "";
+
+    for (let i = letters.length - 1; i >= 0; i--) {
+        invertedText += letters[i];
+    }
+    return invertedText;
+}
+
+console.log(textInverter("Hola Mundo"));
