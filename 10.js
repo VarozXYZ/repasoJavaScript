d720a31172761597e013ee71f9adcd9fd8c75014
VarozXYZ <alvaropiro@gmail.com>
2025-12-02 11:28:34 +0100

    ej10

--- 10.js
+++ 10.js
@@ -11,3 +11,34 @@
 Entrada: arr = [5, 2, 9, 1, 6]
 orden = "Asc"
 Salida: [1, 2, 5, 6, 9] */
+
+function ordenarArray(arr, orden) {
+    let resultado = [];
+    for (let i = 0; i < arr.length; i++) {
+        resultado.push(arr[i]);
+    }
+    
+    for (let i = 0; i < resultado.length - 1; i++) {
+        for (let j = 0; j < resultado.length - 1 - i; j++) {
+            let debeIntercambiar = false;
+            
+            if (orden === "Asc") {
+                debeIntercambiar = resultado[j] > resultado[j + 1];
+            } else if (orden === "Desc") {
+                debeIntercambiar = resultado[j] < resultado[j + 1];
+            }
+            
+            if (debeIntercambiar) {
+                let temp = resultado[j];
+                resultado[j] = resultado[j + 1];
+                resultado[j + 1] = temp;
+            }
+        }
+    }
+    
+    return resultado;
+}
+
+console.log(ordenarArray([5, 2, 9, 1, 6], "Asc"));
+console.log(ordenarArray([5, 2, 9, 1, 6], "Desc"));
+
